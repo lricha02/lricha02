433e8fd18cce5083e6283ac1bb67bec318df1465
Lachlan Richards <lricha02@cgstudents.catholic.edu.au>
2019-09-25 14:12:32 +1000

    PHP start, so much tbh

---
+++ Custom1.php
@@ -0,0 +1,106 @@
+<!-- This is the drink selection page of CustomiseMyCoffee -->
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>DesignMyCoffee</title>
+   <!-- <link rel="stylesheet/less" type="text/css" href="DMCIT.less" /> -->
+    <!-- <script src="less.js" type="text/javascript"></script> -->
+    <link rel="stylesheet" type="text/css" href="DMCIT.css">
+    <link href="https://fonts.googleapis.com/css?family=Livvic&display=swap" rel="stylesheet">
+    <link rel="shortcut icon" type="image/png" href="PicBill.png">
+  </head>
+  <body>
+    <header>
+      <div id='navbar'>
+        <div id='logoBill'><img src="PicBill.png" alt="LogoBill" width="40%" height="70%"></div>
+        <div class='navitem'><a href="index.html">Home</a></div>
+        <div class='navitem'><a href="About.html">About Us</a></div>
+        <div class='navitem'><a href="Locations.html">Locations</a></div>
+        <div class='navitem'><a href="LogIn.php">Log In</a></div>
+      </div>
+    </header>
+    <main>
+      <div id='CustomLand'>
+        <div id='CustomLandLeft'>
+          <h2 class='SubHead'>Choose a Drink</h2>
+            <div id='DrinkGrid'>
+              <button class='DrinkGridItem'><a href='idk.html'>Coffee</a></button>
+              <button class='DrinkGridItem'><a href='idk.html'>Tea</a></button>
+              <button class='DrinkGridItem'><a href='idk.html'>Hot Chocolate</a></button>
+              <button class='DrinkGridItem'><a href='idk.html'>Milkshake</a></button>
+              <button class='DrinkGridItem'><a href='idk.html'>Juice</a></button>
+              <button class='DrinkGridItem'><a href='idk.html'>Soft Drink</a></button>
+            </div>
+          <h4 style="padding-left: 20pt;">Please note that at this time, only coffee is avaliable through the Customise My Coffee system</h4>
+
+          <h2 class='SubHead'>Base Coffee</h2>
+            <div id='DrinkGrid'>
+              <button class='DrinkGridItem'><a href='idk.html'>Flat White</a></button>
+              <button class='DrinkGridItem'><a href='idk.html'>Cappuccino</a></button>
+              <button class='DrinkGridItem'><a href='idk.html'>Latte</a></button>
+              <button class='DrinkGridItem'><a href='idk.html'>Esspresso / Long Black</a></button>
+              <button class='DrinkGridItem'><a href='idk.html'>Chai Latte</a></button>
+              <button class='DrinkGridItem'><a href='idk.html'>Hot Chocolate</a></button>
+            </div>
+
+          <h2 class='SubHead'>Choose a Milk</h2>
+            <div id='DrinkGrid'>
+              <button class='DrinkGridItem'><a href='idk.html'>Full Cream</a></button>
+              <button class='DrinkGridItem'><a href='idk.html'>Skim Milk</a></button>
+              <button class='DrinkGridItem'><a href='idk.html'>Soy Milk</a></button>
+              <button class='DrinkGridItem'><a href='idk.html'>Almond Milk</a></button>
+              <button class='DrinkGridItem'><a href='idk.html'>Lactose Free</a></button>
+              <button class='DrinkGridItem'><a href='idk.html'>No Milk</a></button>
+            </div>
+
+          <h2 class='SubHead'>Size</h2>
+            <div id='DrinkGrid'>
+              <button class='DrinkGridItem'><a href='idk.html'>Small</a></button>
+              <button class='DrinkGridItem'><a href='idk.html'>Large</a></button>
+            </div>
+
+          <h2 class='SubHead'>Strength</h2>
+            <div id='DrinkGrid'>
+              <button class='DrinkGridItem'><a href='idk.html'>Single Shot</a></button>
+              <button class='DrinkGridItem'><a href='idk.html'>Double Shot</a></button>
+              <button class='DrinkGridItem'><a href='idk.html'>No Coffee</a></button>
+            </div>
+
+            <h2 class='SubHead'>Temperature</h2>
+              <div id='DrinkGrid'>
+                <button class='DrinkGridItem'><a href='idk.html'>Default</a></button>
+                <button class='DrinkGridItem'><a href='idk.html'>Hotter</a></button>
+                <button class='DrinkGridItem'><a href='idk.html'>Warmer</a></button>
+              </div>
+
+            <h2 class='SubHead'>Modifiers</h2>
+              <div id='DrinkGrid'>
+                <button class='DrinkGridItem'><a href='idk.html'>One Sugar</a></button>
+                <button class='DrinkGridItem'><a href='idk.html'>Two Sugars</a></button>
+                <button class='DrinkGridItem'><a href='idk.html'>Pouring Cream</a></button>
+                <button class='DrinkGridItem'><a href='idk.html'>Long Black</a></button>
+                <button class='DrinkGridItem'><a href='idk.html'>Chai</a></button>
+                <button class='DrinkGridItem'><a href='idk.html'>Chocolate Powder</a></button>
+              </div>
+
+            <h4>here would be the other notes AND locations!!</h4>
+            <submit>submit goes here</submit>
+          </div>
+        <div id='CustomLandRight'>
+          <img class="bannerImg" alt="banner Image 1" src="Snapchat-1200522755.jpg">
+          <img src="PicBill.png" class="bannerBILL" alt="Picolo Bill">
+          <div class='BillBannerText'>
+          <h6>Picolo Bill Says:</h6>
+          <h6>Always look both ways before crossing the road!</h6>
+        </div>
+        </div>
+      </div>
+    </main>
+    <footer>
+      <div id='footerNav'>
+        <h4 class="footItem">2019 All Rights Reserved, <i>DesignMyCoffee</i></h4>
+        <h4 class="footItem"></h4>
+      </div>
+    </footer>
+  </body>
+</html>
